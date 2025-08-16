cd0c77218a5d9af18fdeb9b798fb08bbb64a1904
Emmanuel N. C. Brito <code@yet1dev.com>
2025-08-16 00:57:41 -0300

    [conf] layerino >> Add basic includes

---
+++ src/src.ino
@@ -0,0 +1,19 @@
+//================================
+// ARDUINO EXECUTION ORDER
+//  - src.ino   (project.ino)
+//  - Layer.ino (alphabetical)
+//  - main.ino  (alphabetical)
+//================================
+#include <any>
+#include <set>
+#include <list>
+#include <array>
+#include <tuple>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <unordered_set>
+#include <unordered_map>
