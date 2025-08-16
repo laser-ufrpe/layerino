7ec78157758ff182b1dc8f79d5dbc6b38335badf
Emmanuel N. C. Brito <code@yet1dev.com>
2025-08-16 00:59:00 -0300

    [feat] layerino >> Add generics type definitions

---
+++ src/L0_types.ino
@@ -0,0 +1,5 @@
+// GENERICS
+#define gent_t template<typename T>
+#define gents_t template<typename... Ts>
+#define gentn_t template<typename T, usize_t N >
+#define genkv_t template<typename K, typename V>
