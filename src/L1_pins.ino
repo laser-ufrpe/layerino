68d3413b51dc14c6dfcda3c8d5f33dbb20e7fb1a
Emmanuel N. C. Brito <code@yet1dev.com>
2025-08-16 02:13:51 -0300

    [feat] layerino >> Add pinmodes and outputs

--- src/L1_pins.ino
+++ src/L1_pins.ino
@@ -1,19 +1,33 @@
 class PIN {
 public:
-  usize_t pin;
-  PIN(usize_t arg) : pin(arg) {}
+  usize_t gpio;
+  usize_t ledc;
+  PIN(usize_t arg) : gpio(arg) {}
+
+  void input()     const { pinMode(gpio, INPUT); }
+  void output()    const { pinMode(gpio, OUTPUT); }
+  void pullup()    const { pinMode(gpio, INPUT_PULLUP); }
+  void pulldown()  const { pinMode(gpio, INPUT_PULLDOWN); }
+  void opendrain() const { pinMode(gpio, OUTPUT_OPEN_DRAIN); }
+
+  void low()             const { digitalWrite(gpio, LOW); }
+  void high()            const { digitalWrite(gpio, HIGH); }
+  void dac(usize_t val)  const { dacWrite(gpio, val); }
+  void dig(usize_t val)  const { digitalWrite(gpio, val); }
+
+  i32_t adc() const { return analogRead(gpio); }
 };
 //==========================================================
 template<usize_t N>
 class PINS {
 public:
-  arr<PIN, N> pins;
+  arr<PIN, N> gpios;
 
-  gents_t PINS(Ts... args) : pins{ PIN(args)... } {}
+  gents_t PINS(Ts... args) : gpios{ PIN(args)... } {}
 
-  constexpr usize_t size() const           { return pins.size(); }
-  PIN& operator[](usize_t idx)             { return pins[idx]; }
-  const PIN& operator[](usize_t idx) const { return pins[idx]; }
+  constexpr usize_t size() const           { return gpios.size(); }
+  PIN& operator[](usize_t idx)             { return gpios[idx]; }
+  const PIN& operator[](usize_t idx) const { return gpios[idx]; }
 };
 //==========================================================
 gents_t PINS(Ts... args) -> PINS<sizeof...(Ts)>;
