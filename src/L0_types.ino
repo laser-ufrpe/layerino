38350464d223f585af12f19a937a2ba27550729b
Emmanuel N. C. Brito <code@yet1dev.com>
2025-08-16 00:59:36 -0300

    [feat] layerino >> Add primitive type definitions

--- src/L0_types.ino
+++ src/L0_types.ino
@@ -3,3 +3,24 @@
 #define gents_t template<typename... Ts>
 #define gentn_t template<typename T, usize_t N >
 #define genkv_t template<typename K, typename V>
+
+// UNSIGNED
+using u8_t  = std::uint8_t;
+using u16_t = std::uint16_t;
+using u32_t = std::uint32_t;
+using u64_t = std::uint64_t;
+
+// SIGNED
+using i8_t  = std::int8_t;
+using i16_t = std::int16_t;
+using i32_t = std::int32_t;
+using i64_t = std::int64_t;
+
+// FLOAT
+using f32_t   = float;
+using f64_t   = double;
+
+// SPECIAL
+using any_t   = std::any;
+using str_t   = std::string;
+using usize_t = std::size_t; 
