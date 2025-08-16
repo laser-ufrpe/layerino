7ec55630b9c618ed113a006c2bbfc3a1460c9763
Emmanuel N. C. Brito <code@yet1dev.com>
2025-08-16 01:02:44 -0300

    [feat] layerino >> Add pins class to multi-gpio control

--- src/L1_pins.ino
+++ src/L1_pins.ino
@@ -3,3 +3,17 @@ public:
   usize_t pin;
   PIN(usize_t arg) : pin(arg) {}
 };
+//==========================================================
+template<usize_t N>
+class PINS {
+public:
+  arr<PIN, N> pins;
+
+  gents_t PINS(Ts... args) : pins{ PIN(args)... } {}
+
+  constexpr usize_t size() const           { return pins.size(); }
+  PIN& operator[](usize_t idx)             { return pins[idx]; }
+  const PIN& operator[](usize_t idx) const { return pins[idx]; }
+};
+//==========================================================
+gents_t PINS(Ts... args) -> PINS<sizeof...(Ts)>;
